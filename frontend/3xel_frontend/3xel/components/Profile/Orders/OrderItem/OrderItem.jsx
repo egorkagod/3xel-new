8e37d0d38ea1ca7983b5fd4cd6fcddbce59d52ae
seregax <alipkin9820@gmail.com>
2025-07-27 14:16:04 +0300

    fix

--- frontend/3xel_frontend/3xel/components/Profile/Orders/OrderItem/OrderItem.jsx
+++ frontend/3xel_frontend/3xel/components/Profile/Orders/OrderItem/OrderItem.jsx
@@ -2,50 +2,38 @@ import classes from './OrderItem.module.scss'
 
 export default function OrderItem({ order }) {
 
-    let paymentMessage
-    let paymentColor
+
     let orderMessage
     let orderColor
 
-    switch (order.payment_status) {
-        case 'NEW':
-            paymentMessage = 'Не оплачено'
-            paymentColor = '#FF4D4F'
-            break
-        case 'AUTHORIZED':
-            paymentMessage = 'Ожидает подтверждения'
-            paymentColor = '#FFA500'
-            break
-        case 'CONFIRMED':
-            paymentMessage = 'Оплачен'
-            paymentColor = '#52C41A'
-            break
-        case 'REJECTED':
-            paymentMessage = 'Отклонен'
-            paymentColor = '#DC3545'
-            break
-        default:
-            paymentMessage = 'Неизвестный статус'
-            paymentColor = '#6C757D'
-            break
+    const status = {
+        order: order.status,
+        payment: order.payment_status
     }
 
-    switch (order.status) {
-        case 'Created':
-            orderMessage = 'Создан'
-            orderColor = '#6C757D'
-            break
-        case 'Payed':
-            orderMessage = 'Оплачен'
-            orderColor = '#FFA500'
-            break
-        case 'Delivered':
+    switch (status) {
+        case (status.order === 'Delivered'): 
             orderMessage = 'Доставлен'
             orderColor = '#52C41A'
             break
-        case 'Finished':
+        case (status.order === 'Finished'):
             orderMessage = 'Завершен'
             orderColor = '#469121ff'
+        case (status.payment === 'NEW'):
+            orderMessage = 'Создан'
+            orderColor = '#6C757D'
+            break
+        case (status.payment === 'AUTHORIZED'):
+            orderMessage = 'Ожидает подтверждения'
+            orderColor = '#FFA500'
+            break
+        case (status.payment === 'CONFIRMED'):
+            orderMessage = 'Оплачен'
+            orderColor = '#52C41A'
+            break
+        case (status.payment === 'REJECTED'):
+            orderMessage = 'Отклонен'
+            orderColor = '#DC3545'
             break
         default:
             orderMessage = 'Неизвестный статус'
@@ -53,15 +41,16 @@ export default function OrderItem({ order }) {
             break
     }
 
+    
+
     return (
         <div className={classes.globalContainer}>
             <div className={classes.header}>
                 <span>Время создания заказа: {order.created_at}</span>
             </div>
             <div className={classes.main}>
-                <span style={{ color: orderColor }}>Заказ: {orderMessage}</span>
+                <span>Заказ: <span style={{ color: orderColor }}>{orderMessage}</span></span>
                 <span>{order.amount} руб.</span>
-                <span style={{ color: paymentColor }}>Платеж: {paymentMessage}</span>
             </div>
         </div>
     )
