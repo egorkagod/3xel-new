d64446e4d27c0731924d348f288fc64ba13e4f48
seregax <alipkin9820@gmail.com>
2025-07-27 17:13:07 +0300

    fix

--- frontend/3xel_frontend/3xel/components/GoodPage/GoodPage.jsx
+++ frontend/3xel_frontend/3xel/components/GoodPage/GoodPage.jsx
@@ -53,7 +53,7 @@ export default function GoodPage() {
         item.selectedColor === selectedColor &&
         item.selectedSize === selectedSize &&
         item.title === product.name
-    )
+    ).length
 
     return (
         <div className={classes.globalContainer}>
@@ -73,7 +73,7 @@ export default function GoodPage() {
                     </div>
                 </div>
                 <div>
-                    {count ? (<button style={{ display: 'flex', alignItems: 'center', gap: '5px', backgroundColor: 'black', padding: '10px 50px' }} className={classes.addToCartBtn} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>
+                    {count === 0 ? (<button style={{ display: 'flex', alignItems: 'center', gap: '5px', backgroundColor: 'black', padding: '10px 50px' }} className={classes.addToCartBtn} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width='16' height='16'>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                         </svg>
