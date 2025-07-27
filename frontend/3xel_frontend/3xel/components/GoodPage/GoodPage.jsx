b2ee9d21761e6097cd52e1c9ae25a1b99c856d87
seregax <alipkin9820@gmail.com>
2025-07-27 17:42:09 +0300

    fix

--- frontend/3xel_frontend/3xel/components/GoodPage/GoodPage.jsx
+++ frontend/3xel_frontend/3xel/components/GoodPage/GoodPage.jsx
@@ -14,6 +14,9 @@ export default function GoodPage() {
     const [isLoading, setIsLoading] = useState(false)
     const [product, setProduct] = useState({})
     const [hasError, setHasError] = useState(false)
+    const [selectedColor, setSelectedColor] = useState(null)
+    const [selectedSize, setSelectedSize] = useState(null)
+    const [cost, setCost] = useState(null)
 
     useEffect(() => {
         async function fetchData() {
@@ -32,14 +35,23 @@ export default function GoodPage() {
             } catch {
                 toast.error('Ошибка при загрузке товара.')
                 setHasError(true)
+            } finally {
+                setIsLoading(false)
             }
         }
 
         fetchData()
     }, [id])
 
-    const [selectedColor, setSelectedColor] = useState(product.variants ? product.variants[0].color : null)
-    const [selectedSize, setSelectedSize] = useState(product.variants ? product.variants[0].size : null)
+    useEffect(() => {
+        if (product.variants && product.variants.length > 0) {
+            setSelectedColor(product.variants[0].color)
+            setSelectedSize(product.variants[0].size)
+            setCost(product.variants[0].price)
+        }
+    }, [product.variants])
+
+
     const variantId = useMemo(() => {
         if (!product.variants) return null
         const match = product.variants.find(v => v.size === selectedSize && v.color === selectedColor)
@@ -59,11 +71,14 @@ export default function GoodPage() {
         <div className={classes.globalContainer}>
             <div style={{ width: '100%', height: '100%', backgroundImage: variantId ? `url(${product.variants.find(v => v.id === variantId).image})` : 'none', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}></div>
             <div className={classes.infoAboutGood}>
-                <h2>{product.name}</h2>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <h2>{product.name}</h2>
+                    <span>{cost}</span>
+                </div>
                 <div className={classes.sizes}>
                     <span>Выберите высоту модельки:</span>
                     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
-                        {uniqueSizesAndCosts.map((variant, index) => <div key={index} onClick={() => setSelectedSize(variant.size)} style={{ fontSize: '20px', textDecoration: selectedSize === variant.size ? 'underline' : 'none', cursor: 'pointer' }}>{variant.size}</div>)}
+                        {uniqueSizesAndCosts.map((variant, index) => <div key={index} onClick={() => { setSelectedSize(variant.size); setCost(variant.price) }} style={{ fontSize: '20px', textDecoration: selectedSize === variant.size ? 'underline' : 'none', cursor: 'pointer' }}>{variant.size}</div>)}
                     </div>
                 </div>
                 <div className={classes.colors}>
@@ -73,16 +88,16 @@ export default function GoodPage() {
                     </div>
                 </div>
                 <div>
-                    {count === 0 ? (<button style={{ display: 'flex', alignItems: 'center', gap: '5px', backgroundColor: 'black', padding: '10px 50px', width: '40%' }} className={classes.addToCartBtn} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>
+                    {count === 0 ? (<button style={{ display: 'flex', alignItems: 'center', gap: '5px', backgroundColor: 'black', padding: '10px 50px', width: '100%' }} className={classes.addToCartBtn} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width='18' height='18'>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                         </svg>
 
                         В корзину
                     </button>) : (
-                        <div style={{ display: count ? 'flex' : 'none', justifyContent: 'space-between', alignItems: 'center', border: '1px solid black', borderRadius: '100px', padding: '10px 50px', gap: '20px', position: 'relative', backgroundColor: 'black', height: 'fit-content', width: '40%' }}>
+                        <div style={{ display: count ? 'flex' : 'none', justifyContent: 'center', alignItems: 'center', border: '1px solid black', borderRadius: '100px', padding: '10px 50px', gap: '20px', position: 'relative', backgroundColor: 'black', height: 'fit-content', width: '100%' }}>
                             <button style={{ all: 'unset', color: 'red', cursor: 'pointer', fontSize: '20px', position: 'absolute', top: '50%', left: '20px', transform: 'translateY(-50%)' }} onClick={(e) => { dispatch(removeFromCart({ title: product.name, selectedSize, selectedColor })); e.stopPropagation() }}>-</button>
-                            <span style={{ fontSize: '12px', color: 'white' }}>{count}</span>
+                            <span style={{ fontSize: '20px', color: 'white' }}>{count}</span>
                             <button style={{ all: 'unset', color: 'green', cursor: 'pointer', fontSize: '20px', position: 'absolute', top: '50%', right: '20px', transform: 'translateY(-50%)' }} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>+</button>
                         </div>
                     )}
