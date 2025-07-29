360cb79f623235adf2245a52c03e6e55c857ca59
seregax <alipkin9820@gmail.com>
2025-07-29 20:53:45 +0300

    update

--- frontend/3xel_frontend/3xel/components/GoodPage/GoodPage.jsx
+++ frontend/3xel_frontend/3xel/components/GoodPage/GoodPage.jsx
@@ -88,14 +88,14 @@ export default function GoodPage() {
                     </div>
                 </div>
                 <div>
-                    {count === 0 ? (<button style={{ display: 'flex', alignItems: 'center', gap: '5px', backgroundColor: 'black', padding: '10px 0', width: '100%' }} className={classes.addToCartBtn} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>
+                    {count === 0 ? (<button style={{ display: 'flex', alignItems: 'center', gap: '5px', backgroundColor: 'black', padding: '10px 0', width: '50%' }} className={classes.addToCartBtn} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width='18' height='18'>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                         </svg>
 
                         В корзину
                     </button>) : (
-                        <div style={{ display: count ? 'flex' : 'none', justifyContent: 'center', alignItems: 'center', border: '1px solid black', borderRadius: '100px', padding: '10px 0', gap: '20px', position: 'relative', backgroundColor: 'black', height: 'fit-content', width: '100%' }}>
+                        <div style={{ display: count ? 'flex' : 'none', justifyContent: 'center', alignItems: 'center', border: '1px solid black', borderRadius: '100px', padding: '10px 0', gap: '20px', position: 'relative', backgroundColor: 'black', height: 'fit-content', width: '50%' }}>
                             <button style={{ all: 'unset', color: 'red', cursor: 'pointer', fontSize: '20px', position: 'absolute', top: '50%', left: '20px', transform: 'translateY(-50%)' }} onClick={(e) => { dispatch(removeFromCart({ title: product.name, selectedSize, selectedColor })); e.stopPropagation() }}>-</button>
                             <span style={{ fontSize: '20px', color: 'white' }}>{count}</span>
                             <button style={{ all: 'unset', color: 'green', cursor: 'pointer', fontSize: '20px', position: 'absolute', top: '50%', right: '20px', transform: 'translateY(-50%)' }} onClick={(e) => { dispatch(addToCart({ id: variantId, title: product.name, background: product.variants.find(v => v.id === variantId).image, selectedColor, selectedSize, cost: product.variants.find(v => v.id === variantId).price })); e.stopPropagation() }}>+</button>
