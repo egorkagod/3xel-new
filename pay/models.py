c82ca46514f586412c138bb8c2f3a1d1a22d2d40
Egor <egor@MacBook-Air-Egor.local>
2025-07-30 21:24:12 +0300

    update

--- pay/models.py
+++ pay/models.py
@@ -19,4 +19,4 @@ class Payment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.id} - {self.status} - {self.amount}руб'
+        return f'{self.id} - {self.get_status_display()} - {self.amount}руб'
--- pay/views.py
+++ pay/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -15,4 +16,4 @@ class NotificationView(APIView):
         data = request.data
         pay_logger.info(f"{data}")
         pay_service.update_status(data)
-        return Response(status=status.HTTP_200_OK)
+        return HttpResponse("OK", status=status.HTTP_200_OK)
