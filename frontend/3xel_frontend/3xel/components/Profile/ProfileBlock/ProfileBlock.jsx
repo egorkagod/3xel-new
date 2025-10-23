d9a33506f73becd22a70ca0091fa920d818e6af2
seregax <alipkin9820@gmail.com>
2025-10-23 22:23:48 +0300

    update

--- frontend/3xel_frontend/3xel/components/Profile/ProfileBlock/ProfileBlock.jsx
+++ frontend/3xel_frontend/3xel/components/Profile/ProfileBlock/ProfileBlock.jsx
@@ -150,7 +150,7 @@ export default function ProfileBlock({
           <h5>Ваши данные</h5>
           <div className={classes.infoRow}>
             <span>Имя</span>
-            <strong>{user?.first_name || 'Не указано'}</strong>
+            <strong>{user?.first_name}</strong>
           </div>
           <div className={classes.infoRow}>
             <span>E-mail</span>
