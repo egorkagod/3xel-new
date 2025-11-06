ba5adea85c0f1734a2175259887f31480f63a7dd
seregax <alipkin9820@gmail.com>
2025-11-06 21:48:52 +0300

    update

--- frontend/3xel_frontend/3xel/components/Constructor/OrderForm/OrderForm.jsx
+++ frontend/3xel_frontend/3xel/components/Constructor/OrderForm/OrderForm.jsx
@@ -33,12 +33,7 @@ export default function OrderForm() {
         const initWidget = () => {
             if (!cdekRef.current) return
             widgetRef.current = new window.CDEKWidget({
-                from: {
-                    country_code: "RU",
-                    city: "Москва",
-                    postal_code: 109518,
-                    address: "2-й Грайвороновский проезд, д. 42к4"
-                },
+                from: "Москва",
                 root: 'cdek-map',
                 apiKey: "6510b8f8-7dc7-4cd4-a94e-1765017a6ded",
                 defaultLocation: "Москва",
@@ -49,6 +44,7 @@ export default function OrderForm() {
                 currency: "RUB",
                 fixBounds: "locality",
                 onCalculate(tariff) {
+                    console.log(tariff)
                     setDeliveryCost(tariff.delivery_sum)
                 },
                 onChoose(mode, tariff, address) {
