093a866b859bc13a5dcb96c8f563267c18adbef2
seregax <alipkin9820@gmail.com>
2025-11-09 01:08:54 +0300

    update

--- frontend/3xel_frontend/3xel/components/Constructor/OrderForm/OrderForm.jsx
+++ frontend/3xel_frontend/3xel/components/Constructor/OrderForm/OrderForm.jsx
@@ -41,7 +41,7 @@ export default function OrderForm() {
                     country_code: "RU",
                     city: "Москва",
                     postal_code: "109518",
-                    address: "ул. 2-й Грайвороновский проезд, д. 42к4"
+                    address: "ул. 2-й Грайвороновский проезд, д. 42к4",
                 },
                 root: 'cdek-map',
                 apiKey: "6510b8f8-7dc7-4cd4-a94e-1765017a6ded",
