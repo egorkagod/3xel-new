5da33531db6fbdf31f91ccf5089c731899321086
Egor <egor@MacBook-Air-Egor.local>
2025-11-09 23:35:47 +0300

    update

--- order/models.py
+++ order/models.py
@@ -73,7 +73,7 @@ class CdekOrder(models.Model):
     email = models.CharField(max_length=100)
     user_fullname = models.CharField(max_length=200)
     tariff_code = models.IntegerField()
-    city_code = models.IntegerField()
+    city_code = models.IntegerField(null=True, default=None)
     city = models.CharField(max_length=200)
     address = models.CharField(max_length=200, default='')
 
