171d7f2dc3e45422a648847af9291b85a70e134e
seregax <alipkin9820@gmail.com>
2025-11-09 21:52:23 +0300

    fix scroll

--- frontend/3xel_frontend/3xel/src/main.jsx
+++ frontend/3xel_frontend/3xel/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import 'react-toastify/dist/ReactToastify.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, ScrollRestoration } from 'react-router-dom'
 import { store } from '../store/store.js'
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <ScrollRestoration></ScrollRestoration>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
