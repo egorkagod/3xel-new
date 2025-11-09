1e450961b49d0b08d979b98b5149fe43962816ee
Egor <egor@MacBook-Air-Egor.local>
2025-11-09 16:35:00 +0300

    update

--- order/repositories/order_rep.py
+++ order/repositories/order_rep.py
@@ -5,7 +5,7 @@ import shutil
 
 from order.models import Order, OrderItem, GoodVariant
 from filehandler.models import File
-from order.dto import CreateOrdeRepoDTO
+from order.dto.order import CreateOrdeRepoDTO
 
 
 def create(data: CreateOrdeRepoDTO) -> int | None:
@@ -17,7 +17,7 @@ def create(data: CreateOrdeRepoDTO) -> int | None:
             video=video,
             comment=(data.comment or ''),
             phone=data.phone,
-            address=data.address,
+            address=data.full_address,
         )
         for item in data.items:
             good_variant = GoodVariant.objects.filter(pk=item.good_variant_id).first()
