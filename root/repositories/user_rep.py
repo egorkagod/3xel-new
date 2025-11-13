a87bc92e47d062999dc34e5270c17b25e51860a0
Egor <egor@MacBook-Air-Egor.local>
2025-11-14 01:22:31 +0300

    update

--- root/repositories/user_rep.py
+++ root/repositories/user_rep.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 def get_or_create(**kwargs):
     username = kwargs.get('username')
