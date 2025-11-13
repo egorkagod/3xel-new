35c4f3d921d2f77325580134983cd768d53163cf
seregax <alipkin9820@gmail.com>
2025-11-13 16:56:16 +0300

    fix

--- frontend/3xel_frontend/3xel/components/MainPage/CertificatesSection/CertificatesSection.jsx
+++ frontend/3xel_frontend/3xel/components/MainPage/CertificatesSection/CertificatesSection.jsx
@@ -1,15 +1,19 @@
 import classes from './CertificatesSection.module.scss'
 import SectionHeader from '../../SectionHeader/SectionHeader'
 import Certificate from '../../Constructor/SelectGood/Certificates/Certificate'
+import { useSelector } from 'react-redux'
 
 export default function CertificatesSection() {
+
+    const certificates = useSelector((state) => state.goods.certificates)
+
     return (
         <section className={classes.certificatesSection} id='certificates'>
             <SectionHeader header='Подарочные сертификаты'>
                 Изготовление, упаковка и доставка включены.
             </SectionHeader>
             <div className={classes.certificatesContainer}>
-                <Certificate isPrototype={true}></Certificate>
+                <Certificate isPrototype={true} certificate={certificates?.[0]}></Certificate>
             </div>
         </section>
     )
