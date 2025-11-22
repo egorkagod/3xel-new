dbbf24d7d4075c654b989376df37d9745a3ce82b
Egor <egor@MacBook-Air-Egor.local>
2025-11-22 21:10:16 +0300

    update

--- cdek/services/cdek_service.py
+++ cdek/services/cdek_service.py
@@ -125,7 +125,7 @@ def register_order(dto: CdekOrderRegisterDTO):
         }
     response = requests.post(url, headers=headers, json=payload)
     if response.status_code != 202:
-        raise CdekBadRequest(f'Ошибка при регистрации заказа в сдэке: {response.text}')
+        raise CdekBadRequest(f'Ошибка при регистрации заказа в сдэке ({response.status_code}): {response.text}')
     resp = response.json()
     return resp
 
