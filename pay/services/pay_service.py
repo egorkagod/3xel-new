3251a16cc712cda22b15e375318ddb14b207fb64
Egor <egor@MacBook-Air-Egor.local>
2025-11-22 22:50:53 +0300

    update

--- pay/services/pay_service.py
+++ pay/services/pay_service.py
@@ -126,13 +126,14 @@ def create_receipt_items(
         for item in unit_items:
             if promocode_amount == 0:
                 items.append(item)
-            
-            if item['Price'] <= promocode_amount:
-                promocode_amount -= item['Price']
             else:
-                item['Price'] -= promocode_amount
-                promocode_amount = 0
-                items.append(item)
+                if item['Price'] <= promocode_amount:
+                    promocode_amount -= item['Price']
+                else:
+                    item['Price'] -= promocode_amount
+                    item['Amount'] = item['Price'] * item['Quantity']
+                    promocode_amount = 0
+                    items.append(item)
     else:
         items = unit_items
 
