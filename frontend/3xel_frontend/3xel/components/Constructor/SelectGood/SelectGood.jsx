325c05018496dfaf6bc3e2bbe44b8f0f09e1050c
seregax <alipkin9820@gmail.com>
2025-11-22 23:56:36 +0300

    update

--- frontend/3xel_frontend/3xel/components/Constructor/SelectGood/SelectGood.jsx
+++ frontend/3xel_frontend/3xel/components/Constructor/SelectGood/SelectGood.jsx
@@ -8,7 +8,6 @@ export default function SelectGood() {
     const certificates = useSelector((state) => state.goods.certificates)
     const goodsStatus = useSelector((state) => state.goods.status)
     const goodsError = useSelector((state) => state.goods.error)
-    const reverseGoods = goods.reverse()
     
     const renderGoods = () => {
         if (goodsStatus === 'loading') {
@@ -23,7 +22,7 @@ export default function SelectGood() {
             return <span className={classes.stateMessage}>Каталог пока пуст.</span>
         }
 
-        const resultGoods = reverseGoods.reduce((acc, cur) => {
+        const resultGoods = goods.reduce((acc, cur) => {
             const newKey = cur.name
             if (!acc[newKey]) {
                 acc[newKey] = []
